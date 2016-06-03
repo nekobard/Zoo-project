f51830a53105a10aa615683b07fbc393cfcfe9c2
Tadeusz <tadeusz.m.lewandowski@gmail.com>
2016-06-03 21:56:07 +0200

    Fix string equals problem and now creating new enclosures works fine

--- src/com/zoo/App.java
+++ src/com/zoo/App.java
@@ -12,30 +12,47 @@ public class App {
 
     private void recognizeCommand(String userCommand){
         String[] commands = userCommand.split("\\s+");
-        if(commands[0] == "new"){
-            if(commands[1] == "enclosure"){
+        if(commands[0].equals("new")){
+            if(commands[1].equals("enclosure")){
                 for(int i = 2; i < commands.length; i++){
-                    int j=0;
-                    boolean nameFound = false;
-                    while(j < this.enclosures.size() && !nameFound){
-                        if(this.enclosures.get(j).getName() == commands[i]){
-                            nameFound = true;
-                        }
-                    }
-                    if(!nameFound){
-                        Enclosure enclosure = new Enclosure(commands[i]);
-                        enclosures.add(enclosure);
-                        System.out.format("Stworzono wybieg %s", commands[i]);
-                    }else{
-                        System.out.format("Wybieg %s juz istnieje\n", commands[i]);
-                    }
-
+                    this.addEnclosure(commands[i]);
                 }
-            } else if(commands[1] == "animal"){
+            } else if(commands[1].equals("animal")){
 
 
 
             }
+        } else if(commands[0].equals("get")){
+            if(commands[1].equals("enclosures")){
+                this.getEnclosures();
+            }
+        } else if(commands[0].equals("exit")){
+            System.out.println("Papa");
+        }
+    }
+
+    private void getEnclosures(){
+        System.out.println("Wybiegi:");
+        this.enclosures.forEach(enclosure -> {
+            System.out.println(enclosure.getName());
+        });
+    }
+
+    private void addEnclosure(String enclosureName){
+        int j=0;
+        boolean nameFound = false;
+        while(j < this.enclosures.size() && !nameFound){
+            if(this.enclosures.get(j).getName().equals(enclosureName)){
+                nameFound = true;
+            }
+            j++;
+        }
+        if(!nameFound){
+            Enclosure enclosure = new Enclosure(enclosureName);
+            enclosures.add(enclosure);
+            System.out.format("Stworzono wybieg %s\n", enclosureName);
+        }else{
+            System.out.format("Wybieg %s juz istnieje\n", enclosureName);
         }
     }
 
@@ -46,15 +63,18 @@ public class App {
     public void start(){
         System.out.println("Witaj w wirtualnym Zoo. Mozesz tworzyc nowe wybiegi i umieszczac tam zwierzeta");
 
-        String userCommand;
-
+        String userCommand = "";
         Scanner sc = new Scanner(System.in);
 
-        userCommand = sc.nextLine();
+        while(!userCommand.equals("exit")){
+            userCommand = sc.nextLine();
+            //System.out.println(userCommand);
+            this.recognizeCommand(userCommand);
+
+        }
 
         sc.close();
 
-        System.out.println(userCommand);
     }
 
     private void manual(){
